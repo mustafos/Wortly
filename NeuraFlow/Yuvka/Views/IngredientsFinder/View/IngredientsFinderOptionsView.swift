cbb064b2f1a79b4f6b66fcf550762c25812a1bc5
mustafos <mustafos1999@icloud.com>
2024-11-30 14:18:12 +0100

    feat: ingredients finder options view

---
+++ NeuraFlow/Yuvka/Views/IngredientsFinder/View/IngredientsFinderOptionsView.swift
@@ -0,0 +1,105 @@
+//
+//  IngredientsFinderOptionsView.swift
+//  Yuvka
+//
+//  Created by Mustafa Bekirov on 30.11.2024.
+//
+
+import SwiftUI
+
+struct IngredientsFinderOptionsView: View {
+    @State var isCameraOpen: Bool = false
+    @State var isSearchViewOpen: Bool = false
+    @State var isSearchViewOpenByIngredients: Bool = false
+    
+    let vm = IngredientsFinderOptionsViewModel()
+    
+    let IngredientsFinderVCWrapper = UIKitViewControllerWrapper(viewController: IngredientsFinderWithCameraViewController())
+    
+    var body: some View {
+        VStack {
+            VStack {
+                Spacer()
+                VStack(spacing: 0) {
+                    VStack(alignment: .leading) {
+                        HStack {
+                            Text("Find recipes that you desire")
+                                .font(.custom("Poppins-Medium", size: 31))
+                            
+                            Spacer()
+                            
+                            Image(systemName: "fork.knife.circle")
+                                .font(.system(size: 58))
+                        }
+                    }
+                    .padding([.horizontal],20)
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                    
+                    // MARK: Options Grid View
+                    VStack {
+                        VStack {
+                            HStack(spacing: 10) {
+                                LargeSizeOptionBox(
+                                    title: "Search an image in real time",
+                                    bgColor: .indigo,
+                                    icon: "camera",
+                                    action: {
+                                        isCameraOpen.toggle()
+                                    }
+                                )
+                                
+                                VStack(spacing: 10) {
+                                    MediumSizeOptionBox(
+                                        title: "Search By Ingredients",
+                                        bgColor: Color.accentColor,
+                                        icon: "takeoutbag.and.cup.and.straw",
+                                        action: {
+                                            isSearchViewOpen_ByIngredients
+                                                .toggle()
+                                        }
+                                    )
+                                    
+                                    MediumSizeOptionBox(
+                                        title: "Search a specific recipe",
+                                        bgColor: .akBg,
+                                        icon: "magnifyingglass",
+                                        action: {
+                                            isSearchViewOpen.toggle()
+                                        }
+                                    )
+                                }
+                            }
+                        }
+                        .padding(10)
+                        .frame(width: 350, height: 360)
+                    }
+                    .frame(maxWidth: .infinity, alignment: .center)
+                }
+                .padding(.top, 10)
+                
+                Spacer()
+            }
+            .padding(.top, 45)
+        }
+        .fullScreenCover(
+            isPresented: $isSearchViewOpenByIngredients,
+            content: {
+                SearchByIngredientsView()
+            }
+        )
+        .fullScreenCover(isPresented: $isSearchViewOpen, content: {
+            SearchRecipeByNameView(viewModel: vm)
+                .presentationDetents([.large])
+                .presentationDragIndicator(.visible)
+        })
+        .fullScreenCover(isPresented: $isCameraOpen, content: {
+            IngredientsFinderVCWrapper
+                .presentationBackground(.black)
+        })
+        .ignoresSafeArea()
+    }
+}
+
+#Preview {
+    IngredientsFinderOptionsView()
+}
