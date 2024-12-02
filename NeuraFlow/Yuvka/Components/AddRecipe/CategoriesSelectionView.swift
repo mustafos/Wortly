7e07739e8cf56ecd7f3dfe08947edf47f974cdb7
mustafos <mustafos1999@icloud.com>
2024-12-02 18:13:45 +0100

    feat: categories selection view

---
+++ NeuraFlow/Yuvka/Components/AddRecipe/CategoriesSelectionView.swift
@@ -0,0 +1,35 @@
+//
+//  CategoriesSelectionView.swift
+//  Yuvka
+//
+//  Created by Mustafa Bekirov on 02.12.2024.
+//
+
+import SwiftUI
+
+struct CategoriesSelectionView: View {
+    @Binding var category: [String]
+    
+    var body: some View {
+        ScrollView(.horizontal) {
+            HStack(spacing: 8) {
+                ForEach(Category.allCases) { catg in
+                    VStack {
+                        Text("\(catg.rawValue)")
+                            .padding(.horizontal, 8)
+                            .frame(width: 100, height: 40, alignment: .center)
+                            .background(.akBg)
+                            .clipShape(RoundedRectangle(cornerRadius: 18))
+                    }
+                    .onTapGesture {
+                        category.append(catg.rawValue)
+                    }
+                }
+            }
+        }.padding(8)
+    }
+}
+
+#Preview {
+    CategoriesSelectionView(category: .constant([""]))
+}
