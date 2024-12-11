c416374b66d0328a247ca53a5cde2a2ed55fc9b5
mustafos <mustafos1999@icloud.com>
2024-12-11 18:34:12 +0100

    init WortlyApp

---
+++ wortly/wortly/WortlyApp.swift
@@ -0,0 +1,17 @@
+//
+//  WortlyApp.swift
+//  wortly
+//
+//  Created by Mustafa Bekirov on 20.11.2024.
+//
+
+import SwiftUI
+
+@main
+struct WortlyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
