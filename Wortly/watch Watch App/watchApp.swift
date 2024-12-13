d92fbba5c3bc1a23abddae40cbb7858faae50dab
mustafos <mustafos1999@icloud.com>
2024-12-13 10:23:12 +0100

    feat: widget and watch target with minimum deploy

---
+++ Wortly/watch Watch App/watchApp.swift
@@ -0,0 +1,17 @@
+//
+//  watchApp.swift
+//  watch Watch App
+//
+//  Created by Mustafa Bekirov on 20.04.2025.
+//
+
+import SwiftUI
+
+@main
+struct watch_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
