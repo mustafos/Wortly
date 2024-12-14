e8901e5867a18871097f0cb53d946317bc683dce
mustafos <mustafos1999@icloud.com>
2024-12-14 12:09:01 +0100

    feat: setting view

---
+++ Wortly/Wortly/Views/SettingsView.swift
@@ -0,0 +1,18 @@
+//
+//  SettingsView.swift
+//  wortly
+//
+//  Created by Mustafa Bekirov on 15.12.2024.
+//
+
+import SwiftUI
+
+struct SettingsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SettingsView()
+}
