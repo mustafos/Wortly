41c52c6b422f97b8b9b7d3596ef39c8b1d7d6d21
mustafos <mustafos1999@icloud.com>
2024-12-15 14:12:34 +0100

    feat: settings navigation view

--- Wortly/Wortly/Views/SettingsView.swift
+++ Wortly/Wortly/Views/SettingsView.swift
@@ -8,11 +8,53 @@
 import SwiftUI
 
 struct SettingsView: View {
+    
+    @ObservedObject var viewModel: SettingsViewModel
+    @Binding var showingSettings: Bool
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView {
+            VStack(alignment: .leading, spacing: 20) {
+                List {
+                    Section(footer: footer) {
+                        ForEach(viewModel.items, id: \.rawValue) { item in
+                            Button(action: { handleTapOnItem(item) }) {
+                                SettingRow(item: item)
+                            }
+                        }
+                    }
+                    
+                }
+                .listStyle(InsetGroupedListStyle())
+            }
+            .navigationBarTitle(Text(viewModel.title), displayMode: .inline)
+            .navigationBarItems(leading:
+                Button(action: {
+                    self.showingSettings.toggle()
+                }) {
+                    Text(viewModel.closeText)
+                }
+            )
+        }
     }
-}
+    
+    var footer: some View {
+        Bundle.version
+            .map { Text("app.version.\($0)") }
+            .modifier(FooterStyle())
+    }
+    
+    private func handleTapOnItem(_ item: SettingItem) {
+        switch item {
+        case .feedback: openURL(viewModel.appStoreURL)
+        case .openSource: openURL(viewModel.gitHubRepoURL)
+        }
+    }
 
-#Preview {
-    SettingsView()
+    private func openURL(_ url: URL) {
+        guard UIApplication.shared.canOpenURL(url) else { return }
+        
+        print("SettingsView: openURL \(url.absoluteString)")
+        UIApplication.shared.open(url)
+    }
 }
