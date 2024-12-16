be4bc1d5f6d916f8c28bece82aea2334c6727377
mustafos <mustafos1999@icloud.com>
2024-12-16 23:11:23 +0100

    feat: settings view model

--- Wortly/Wortly/WordlyApp.swift
+++ Wortly/Wortly/WordlyApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct WordlyApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            WordlyView(viewModel: .init())
         }
     }
 }
