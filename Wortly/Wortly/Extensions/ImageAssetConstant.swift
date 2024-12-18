cf11caef9a18c9770a388ab25b7f72b50eae9259
mustafos <mustafos1999@icloud.com>
2024-12-18 18:51:17 +0100

    feat: image asset constant

---
+++ Wortly/Wortly/Extensions/ImageAssetConstant.swift
@@ -0,0 +1,12 @@
+//
+//  ImageAssetConstant.swift
+//  wortly
+//
+//  Created by Mustafa Bekirov on 18.12.2024.
+//
+
+import Foundation
+
+class ImageAssetConstant {
+    static let settings = "Settings"
+}
