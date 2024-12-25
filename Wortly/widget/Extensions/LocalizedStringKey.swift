d1083a9c1f8f96f54edc1256d8a50e127bac6357
mustafos <mustafos1999@icloud.com>
2024-12-25 12:02:09 +0100

    feat: localized string key

---
+++ Wortly/widget/Extensions/LocalizedStringKey.swift
@@ -0,0 +1,22 @@
+//
+//  LocalizedStringKey.swift
+//  wortly
+//
+//  Created by Mustafa Bekirov on 25.12.2024.
+//
+
+import SwiftUI
+
+extension LocalizedStringKey {
+
+    var stringKey: String {
+        let description = "\(self)"
+        let components = description.components(separatedBy: "key: \"").map { $0.components(separatedBy: "\",") }
+
+        return components[1][0]
+    }
+    
+    func stringValue() -> String {
+        return NSLocalizedString(stringKey, comment: "")
+    }
+}
