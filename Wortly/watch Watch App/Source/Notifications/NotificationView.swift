26d6dfb873900c4f1fe7d59cc433aa0b2f15dea6
mustafos <mustafos1999@icloud.com>
2024-12-28 20:04:01 +0100

    feat: notification basic view

---
+++ Wortly/watch Watch App/Source/Notifications/NotificationView.swift
@@ -0,0 +1,20 @@
+//
+//  NotificationView.swift
+//  watch Watch App
+//
+//  Created by Mustafa Bekirov on 28.12.2024.
+//
+
+import SwiftUI
+
+struct NotificationView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct NotificationView_Previews: PreviewProvider {
+    static var previews: some View {
+        NotificationView()
+    }
+}
